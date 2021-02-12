89f67da6164346b103cf2aeef8fdac8cb4071768
HemaSathiz <hema.sathizkumar@gmail.com>
2021-02-12 10:09:55 +0530

    Modified the UI based on the material design

--- app/src/main/java/com/persistent/microsoftassignment/ui/main/videos/ItemVideoDetailsListViewModel.kt
+++ app/src/main/java/com/persistent/microsoftassignment/ui/main/videos/ItemVideoDetailsListViewModel.kt
@@ -3,19 +3,33 @@ package com.persistent.microsoftassignment.ui.main.videos
 import androidx.databinding.BaseObservable
 import androidx.databinding.ObservableField
 import com.persistent.microsoftassignment.models.Result
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ItemVideoDetailsListViewModel(row: Result) : BaseObservable() {
 
     var title: ObservableField<String> = ObservableField()
-    private var type: ObservableField<String> = ObservableField()
+     var description: ObservableField<String> = ObservableField()
     var imageUrl: ObservableField<String> = ObservableField()
     var year: ObservableField<String> = ObservableField()
+    var raringBar: ObservableField<String> = ObservableField()
+    var vote: ObservableField<String> = ObservableField()
 
     init {
         title.set(row.title)
-        type.set(row.popularity.toString())
-        year.set(row.release_date)
+        description.set(row.overview)
+        year.set(getYearFromDate(row.release_date))
         imageUrl.set(row.poster_path)
+        raringBar.set(row.vote_average.toString())
+        vote.set(row.vote_count.toString())
 
+    }
+
+    fun getYearFromDate(dateString : String): String {
+        val fmt = SimpleDateFormat("yyyy-MM-dd")
+        val date: Date = fmt.parse(dateString)
+
+        val fmtOut = SimpleDateFormat("yyyy")
+        return fmtOut.format(date)
     }
 }
--- app/src/main/java/com/persistent/microsoftassignment/ui/main/videos/VideosFragment.kt
+++ app/src/main/java/com/persistent/microsoftassignment/ui/main/videos/VideosFragment.kt
@@ -9,6 +9,8 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.persistent.microsoftassignment.R
 import com.persistent.microsoftassignment.databinding.FragmentVideosBinding
 import com.persistent.microsoftassignment.models.Movies
@@ -23,7 +25,7 @@ class VideosFragment : Fragment() {
 
     private lateinit var binding: FragmentVideosBinding
     private lateinit var viewModel: VideoViewModel
-    private var viewManager: GridLayoutManager? = null
+    private var viewManager: LinearLayoutManager? = null
     private var viewAdapter: VideoAdapter? = null
     private  var videoDetails: List<Result> = ArrayList()
 
@@ -40,17 +42,14 @@ class VideosFragment : Fragment() {
         binding.lifecycleOwner = this
 
 
-        binding.swipeRefresh.setColorSchemeColors(Color.GRAY, Color.GRAY, Color.GRAY)
 
-        viewManager = GridLayoutManager(activity!!,2)
+        viewManager = LinearLayoutManager(activity!!,LinearLayoutManager.VERTICAL,false);
         viewAdapter = VideoAdapter()
         binding.movieRecyclerView.setHasFixedSize(true)
         binding.movieRecyclerView.layoutManager = viewManager
         binding.movieRecyclerView.adapter = viewAdapter
 
-        binding.swipeRefresh.setOnRefreshListener {
-            viewModel.fetchVideoDetails(activity!!)
-        }
+
         binding.progressBar.visibility = View.VISIBLE
 
         viewModel.getVideoDetails().observe(this, androidx.lifecycle.Observer<Movies> { response ->
@@ -59,9 +58,7 @@ class VideosFragment : Fragment() {
                 movieRecyclerView.visibility = View.VISIBLE
                 videoDetails = response.results
                 progressBar.visibility = View.GONE
-                if (swipeRefresh.isRefreshing) {
-                    swipeRefresh.isRefreshing = false
-                }
+
                 viewModel.addVideoItemsToList(response.results)
             }
 
@@ -70,9 +67,7 @@ class VideosFragment : Fragment() {
         viewModel.fetchVideoDetails(activity!!)
 
         viewModel.getErrorResponseVideoDetails().observe(this, androidx.lifecycle.Observer<String> { response ->
-            if (swipeRefresh.isRefreshing) {
-                swipeRefresh.isRefreshing = false
-            }
+
             progressBar.visibility = View.GONE
             lin_fragment_video_linear_no_network_connection.visibility = View.VISIBLE
             movieRecyclerView.visibility = View.GONE
